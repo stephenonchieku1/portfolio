f45cdfa7a14671536053d709038b8d94704dd4ca
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-02-07 15:22:30 +0300

    feat: changed location and name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className='container flex'>
             <div className='header-content'>
                 <h2 className='text-uppercase text-white op-07 fw-6 ls-2'>web design, branding, graphic</h2>
-                <h1 className='text-white fw-6 header-title'>Hello, I'm <span className='text-brown'>Web & Graphic Designer</span> living in Melbourne, Australia</h1>
+                <h1 className='text-white fw-6 header-title'>Hello, I'm <span className='text-brown'>Web & Graphic Designer</span> living in Nairobi, Kenya</h1>
                 <div className='btn-groups flex'>
                   <button type = "button" className='btn-item bg-brown fw-4 ls-2'>See Works</button>
                   <button type = "button" className='btn-item bg-dark fw-4 ls-2'>Contact Me</button>
