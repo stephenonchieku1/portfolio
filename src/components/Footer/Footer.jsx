c048a038331d3fa408218d82982d93629af380d2
stephenonchieku1 <onchiekustephen901@gmail.com>
2023-04-29 10:56:34 +0300

    minor changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,38 @@
 import React from 'react';
 import "./Footer.css";
-import { FaFacebookF, FaTwitter, FaInstagram, FaPinterest, FaBehance } from 'react-icons/fa';
+import {  FaTwitter, FaInstagram, FaGithubAlt, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <div className='footer bg-dark section-p' id = "footer">
+    <div className='footer bg-dark section-p' id = "footer" >
         <div className='container'>
             <div className='section-title'>
                 <h3 className='text-brown'>Follow <span className='text-white'>Me</span></h3>
             </div>
-            <div className='footer-content'>
-                <ul className='footer-social-links flex flex-c'>
+            <div className='footer-content '>
+                <ul className='footer-social-links flex flex-c '>
                     <li>
-                        <a href = "/" className='text-white'>
-                            <FaFacebookF />
+                        <a href = "https://github.com/stephenonchieku1" className='text-white'>
+                            <FaGithubAlt/>
                         </a>
                     </li>
                     <li>
-                        <a href = "/" className='text-white'>
+                        <a href = "https://twitter.com/StephenOnchieku" className='text-white'>
                             <FaTwitter />
                         </a>
                     </li>
                     <li>
-                        <a href = "/" className='text-white'>
+                        <a href = "https://www.instagram.com/xtrevasone/" className='text-white'>
                             <FaInstagram />
                         </a>
                     </li>
                     <li>
-                        <a href = "/" className='text-white'>
-                            <FaPinterest />
+                        <a href = "https://www.linkedin.com/in/stephen-onchieku-70297121b/" className='text-white'>
+                            <FaLinkedin />
                         </a>
                     </li>
-                    <li>
-                        <a href = "/" className='text-white'>
-                            <FaBehance />
-                        </a>
-                    </li>
+                 
+                 
                 </ul>
                 <p className='text-center fs-14 text-white op-07 fw-3 ls-1 reserved-text'>All Rights Reserved &copy; 2022</p>
             </div>
